4c7ae72f2a6e47585372fe5a6764b23c23e4d950
sultan malik <48114122+sultanmalik04@users.noreply.github.com>
2021-03-11 14:00:32 +0530

    now user is able to comment

--- auctions/views.py
+++ auctions/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Listing, Watchlist
+from .models import User, Listing, Watchlist, Comments
 
 
 def index(request):
@@ -87,15 +87,38 @@ def CreatListingView(request):
 # stop here, to learn BOOTSTRAPE for styling
 
 # deatail view of specified product
-def detailView(request, id):
-    obj = Listing.objects.get(id=id)
-    watchlist_obj = Watchlist.objects.filter(list_id=id, user_id=request.user.id)
-    return render(request, "auctions/product.html", {
-        "product": obj,
-        "watchlist_object": watchlist_obj
-    })
+# latest: added comment functionality, only logged in user can comment
+def detailView(request, id, *argv):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            comment = request.POST["comment"]
+            list_obj = Listing.objects.get(id=id)
+            comment_obj = Comments(comment=comment, user=request.user, listing=list_obj)
+            comment_obj.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        else:
+            return HttpResponseRedirect(reverse("login"))
+    else:
+        comment_obj = Comments.objects.filter(listing=id)
+        obj = Listing.objects.get(id=id)
+        if request.user.is_authenticated:
+            watchlist_obj = Watchlist.objects.filter(list_id=id, user_id=request.user.id)
+            return render(request, "auctions/product.html", {
+                "product": obj,
+                "watchlist_object": watchlist_obj,
+                "comments": comment_obj
+            })
+        else: 
+            return render(request, "auctions/product.html", {
+                "product": obj,
+                "comments": comment_obj,
+                "message": "".join([arg for arg in argv])
+            })
+            
+
 
 # Add list to watchlist
+@login_required(login_url='login')
 def addToWatchlist(request, id):
     current_user_id = request.user.id
     userId = User.objects.get(id = current_user_id)
@@ -120,3 +143,5 @@ def removeFromWatchList(request, id):
     return HttpResponseRedirect(reverse("watchlist"))
 
 
+def commentView(request):
+    return HttpResponse("<h1>Commnted</h1>")
